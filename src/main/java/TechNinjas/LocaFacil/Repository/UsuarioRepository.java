eee45b4b083af13dc27d63798453f11b6adcf00a
Vinicius <ayatogames16@gmail.com>
2021-08-19 19:52:59 -0300

    Primeiro commit

---
+++ src/main/java/TechNinjas/LocaFacil/Repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package TechNinjas.LocaFacil.Repository;
+
+import TechNinjas.LocaFacil.Model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
+
+    public Optional<UsuarioModel> findByEmail(String email);
+}
---
+++ src/test/java/TechNinjas/LocaFacil/LocaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package TechNinjas.LocaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
